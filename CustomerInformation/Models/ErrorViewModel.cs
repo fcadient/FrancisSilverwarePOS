ceb71072f29ca5d4bbb87f604b8cf050ec568ef9
Francis Cadiente <francis.m.p.cadiente@gmail.com>
2021-03-02 20:10:27 -0500

    initial commit
    initial commit

---
+++ CustomerInformation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CustomerInformation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
